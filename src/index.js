b252d589b087f93c3cbaf7112aaeb3d135ee0d90
ekmpoon <ekmpoon@gmail.com>
2023-02-20 23:51:02 +0800

    Ch 183 Complete

--- src/index.js
+++ src/index.js
@@ -14,11 +14,9 @@ const rootElement = document.getElementById('root');
 render(
   <React.StrictMode>
     <Provider store={store}>
-      <PersistGate loading={null} persistor={persistor}>
         <BrowserRouter>
           <App />
         </BrowserRouter>
-      </PersistGate>
     </Provider>
   </React.StrictMode>,
   rootElement
